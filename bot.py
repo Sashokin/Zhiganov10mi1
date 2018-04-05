54ce0fa2b7b27735806895fface02bebe8b0f29a
Alexander <boost.prome@mail.ru>
2018-04-05 17:28:42 +0300

    Приветствует и перенаправляет на сайт

--- bot.py
+++ bot.py
@@ -3,14 +3,19 @@
 import config
 import bottoken
 import telebot
+import time
+from telebot import types
 
 
 bot = telebot.TeleBot(bottoken.mytoken)
 
 
 @bot.message_handler(content_types=["text"])
-def repeat_all_messages(message):
-    bot.send_message(message.chat.id, message.text)
+def default_test(message):
+    keyboard = types.InlineKeyboardMarkup()
+    url_button = types.InlineKeyboardButton(text="Перейти на сайт", url="www.metoyou-shop.ru")
+    keyboard.add(url_button)
+    bot.send_message(message.chat.id, "Привет! Бот находится в стадии проектирования. Нажми на кнопку для перехода на наш сайт.", reply_markup=keyboard)
 
 
 if __name__ == '__main__':
