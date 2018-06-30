497850cd1a03bcc0d4e516b5cc9782a5ec7df262
Alexander <boost.prome@mail>
2018-06-30 20:37:44 +0300

    Перешел на винду + подключил vpn. Добавлен ответ на команды help, start

--- bot.py
+++ bot.py
@@ -7,12 +7,21 @@ from telebot import types
 
 bot = telebot.TeleBot(config.token)
 
+
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    keyboard = types.InlineKeyboardMarkup()
+    url_button = types.InlineKeyboardButton(text="Перейти на сайт", url="www.metoyou-shop.ru")
+    keyboard.add(url_button)
+    bot.send_message(message.chat.id, 'Привет! Я бот магазина metoyou!', reply_markup=keyboard)
+
+
 @bot.message_handler(content_types=["text"])
 def default_test(message):
     keyboard = types.InlineKeyboardMarkup()
     url_button = types.InlineKeyboardButton(text="Перейти на сайт", url="www.metoyou-shop.ru")
     keyboard.add(url_button)
-    bot.send_message(message.chat.id, "Привет! Бот находится в стадии проектирования. Нажми на кнопку для перехода на наш сайт.", reply_markup=keyboard)
+    bot.send_message(message.chat.id, "В данный момент бот находится в стадии проектирования. Нажми на кнопку для перехода на наш сайт.", reply_markup=keyboard)
 
 
 if __name__ == '__main__':
