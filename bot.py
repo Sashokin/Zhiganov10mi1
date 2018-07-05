052e7f34ee984b78ac4019e9f4bae3828888bff2
Alexander <boost.prome@mail>
2018-07-05 16:07:59 +0300

    Добавлен переход на главное меню, ответ на любой тип сообщения

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ import config
 import telebot
 import time
 from telebot import types
-import logging
 
 
 bot = telebot.TeleBot(config.token)
@@ -14,15 +13,34 @@ def send_welcome(message):
     keyboard = types.InlineKeyboardMarkup()
     url_button = types.InlineKeyboardButton(text="Перейти на сайт", url="www.metoyou-shop.ru")
     keyboard.add(url_button)
+    markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    button = types.KeyboardButton('Главное меню')
+    markup.add(button)
     bot.send_message(message.chat.id, 'Привет! Я бот магазина metoyou!', reply_markup=keyboard)
+    bot.send_message(message.chat.id, 'Воспользуйтесь клавиатурой, чтобы перейти в главное меню', reply_markup=markup)
 
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def default_test(message):
-    keyboard = types.InlineKeyboardMarkup()
-    url_button = types.InlineKeyboardButton(text="Перейти на сайт", url="www.metoyou-shop.ru")
-    keyboard.add(url_button)
-    bot.send_message(message.chat.id, "В данный момент бот находится в стадии проектирования. Нажми на кнопку для перехода на наш сайт.", reply_markup=keyboard)
+@bot.message_handler(content_types=["text"])
+def main_menu(message):
+    if message.text == 'Главное меню':
+        bot.send_message(message.chat.id, 'Это главное меню')
+    else:
+        keyboard = types.InlineKeyboardMarkup()
+        url_button = types.InlineKeyboardButton(text="Перейти на сайт", url="www.metoyou-shop.ru")
+        keyboard.add(url_button)
+        markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        button = types.KeyboardButton('Главное меню')
+        markup.add(button)
+        bot.send_message(message.chat.id, "Не понимаю Вас. Нажмите на кнопку для перехода на наш сайт.", reply_markup=keyboard)
+        bot.send_message(message.chat.id, 'Или перейдите в главное меню', reply_markup=markup)
+
+
+@bot.message_handler(content_types=["sticker", "pinned_message", "photo", "audio", 'document'])
+def answer_not_a_text(message):
+    markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    button = types.KeyboardButton('Главное меню')
+    markup.add(button)
+    bot.send_message(message.chat.id, "Не могу никак на это ответить, перейти на главное меню?", reply_markup=markup)
 
 
 if __name__ == '__main__':
