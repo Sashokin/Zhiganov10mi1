3033106ff3fe3a4da222607dcd52e7f16e7327e7
Alexander <boost.prome@mail>
2018-07-07 16:48:35 +0300

    Добавлена страница персональных данных

--- bot.py
+++ bot.py
@@ -17,8 +17,8 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['help'])
 def send_help(message):
-    f = open('commands.txt', encoding='utf-8')
-    bot.send_message(message.chat.id, 'Список команд:\n{}\n\nВыберите ниже раздел справки и получите всю необходимую информацию. Если вопрос не решен, обратитесь сюда: @glhflll'.format(f.read()), reply_markup=my_markups.help_page)
+    comtxt = open('commands.txt', encoding='utf-8')
+    bot.send_message(message.chat.id, 'Список команд:\n\n{}\n\nВыберите ниже раздел справки и получите всю необходимую информацию. Если вопрос не решен, обратитесь сюда: @glhflll'.format(comtxt.read()), reply_markup=my_markups.help_page)
 
 
 @bot.message_handler(commands=['main'])
@@ -26,28 +26,36 @@ def main_menu(message):
     bot.send_message(message.chat.id, 'Главное меню', reply_markup=my_markups.main_menu)
 
 
+@bot.message_handler(commands=['personal'])
+def main_menu(message):
+    bot.send_message(message.chat.id, 'Персональные данные', reply_markup=my_markups.personal_page)
+
+
 @bot.message_handler(content_types=["text"])
 def main_menu(message):
     if message.text == 'Главное меню' or message.text == 'Вернуться в главное меню':
         bot.send_message(message.chat.id, 'Главное меню', reply_markup=my_markups.main_menu)
     elif message.text == 'Помощь и связь' or message.text == 'Помощь':
-        f = open('commands.txt', encoding='utf-8')
-        bot.send_message(message.chat.id, 'Список команд:\n{}\n\nВыберите ниже раздел справки и получите всю необходимую информацию. Если вопрос не решен, обратитесь сюда: @glhflll'.format(f.read()), reply_markup=my_markups.help_page)
+        comtxt = open('commands.txt', encoding='utf-8')
+        bot.send_message(message.chat.id, 'Список команд:\n\n{}\n\nВыберите ниже раздел справки и получите всю необходимую информацию. Если вопрос не решен, обратитесь сюда: @glhflll'.format(comtxt.read()), reply_markup=my_markups.help_page)
     elif message.text == 'Информация на сайте':
         bot.send_message(message.chat.id, 'Если информации на сайте недостаточно, свяжитесь с нами', reply_markup=my_markups.to_site)
         bot.send_message(message.chat.id, 'Связаться с нами', reply_markup=my_markups.help_page_out_site)
     elif message.text == 'Позвонить':
         bot.send_message(message.chat.id, '+7(916)204-12-22\n\nГрафик работы:\nБудни 9:00-21:00\nСуббота 10:00-19:00', reply_markup=my_markups.help_page)
     elif message.text == 'Написать':
-        bot.send_message(message.chat.id,'По всем вопросам: @glhflll', reply_markup=my_markups.help_page)
+        bot.send_message(message.chat.id, 'По всем вопросам: @glhflll', reply_markup=my_markups.help_page)
+    elif message.text == 'Персональные данные':
+        bot.send_message(message.chat.id, 'Персональные данные', reply_markup=my_markups.personal_page)
     else:
-        bot.send_message(message.chat.id, "Не понимаю Вас. Нажмите на кнопку для перехода на наш сайт.", reply_markup=my_markups.to_site)
-        bot.send_message(message.chat.id, 'Или перейдите в главное меню', reply_markup=my_markups.go_to_main_menu)
+        comtxt = open('commands.txt', encoding='utf-8')
+        bot.send_message(message.chat.id, 'Не понимаю Вас, вот список команд:\n\n{}\n\n'.format(comtxt.read()), reply_markup=my_markups.help_page)
 
 
 @bot.message_handler(content_types=["sticker", "pinned_message", "photo", "audio", 'document'])
 def answer_not_a_text(message):
-    bot.send_message(message.chat.id, "Не могу никак на это ответить, перейти в главное меню?", reply_markup=my_markups.go_to_main_menu)
+    comtxt = open('commands.txt', encoding='utf-8')
+    bot.send_message(message.chat.id, 'Не могу никак на это ответить, вот список команд\n\n{}\n\n'.format(comtxt.read()), reply_markup=my_markups.go_to_main_menu)
 
 
 if __name__ == '__main__':
